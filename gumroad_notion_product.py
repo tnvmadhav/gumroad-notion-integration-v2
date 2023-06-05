76eeacc0df84743a6d296dc27f74c800cfc99b7a
tnvmadhav <tnvmadhav@gmail.com>
2023-06-05 08:44:32 +0530

    Message: real v2

--- gumroad_notion_product.py
+++ gumroad_notion_product.py
@@ -1,52 +1,45 @@
 import json
 import time
-from http import HTTPStatus
-from urllib.parse import urlparse
-
 import requests
-import yaml
+import traceback
+import sys
+import os
 
 
 class MyIntegration:
 
     def __init__(self):
         """
-        Gets required variable data from config yaml file.
+        Gets required secrets from environment variables.
         """
-        with open("my_variables.yml", 'r') as stream:
-            try:
-                self.my_variables_map = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print("[Error]: while reading yml file", exc)
-        self.my_variables_map["NOTION_ENTRIES"] = {}
-        self.getPageAndDatabaseData()
-        self.getGumroadUserDetails()
-
-    def getGumroadUserDetails(self):
-        url = "https://api.gumroad.com/v2/user/"
-        headers = {
-            'Authorization': 'Bearer ' +
-            self.my_variables_map["MY_GUMROAD_SECRET_TOKEN"],
+        self.my_variables_map = {
+            "MY_NOTION_SECRET_TOKEN": os.getenv("MY_NOTION_SECRET_TOKEN"),
+            "MY_GUMROAD_SECRET_TOKEN": os.getenv("MY_GUMROAD_SECRET_TOKEN"),
+            "NOTION_ENTRIES": {},
         }
-        response = requests.request("GET", url, headers=headers)
-        if response.status_code == HTTPStatus.OK:
-            self.my_variables_map["GUMROAD_USER"] = {
-                "name": response.json()["user"]["name"],
-                "profile": response.json()["user"]["url"]
-            }
+        self.fallback_cover_image_url = (
+            "https://assets-global.website-files.com/6171b265e5c8aa59b42c3472/6195275a9e5f4655891de886_gum-coins.svg"  # noqa: E501
+        )
+        self.get_page_and_database_data()
 
-    def getPageAndDatabaseData(self):
-        url = "https://api.notion.com/v1/databases/"
+    def get_page_and_database_data(self):
+        url = "https://api.notion.com/v1/search"
         headers = {
-            'Notion-Version': '2021-05-13',
+            "Content-Type": "application/json",
+            'Notion-Version': '2022-02-22',
             'Authorization':
                 'Bearer ' + self.my_variables_map["MY_NOTION_SECRET_TOKEN"]
         }
-        response = requests.request("GET", url, headers=headers)
+        payload = json.dumps({
+            "query": 'Store',
+            "filter": {
+                "value": "database",
+                "property": "object"
+            }
+        })
+        response = requests.request("POST", url, headers=headers, data=payload)
         self.my_variables_map["DATABASE_ID"] = \
             response.json()["results"][0]["id"]
-        self.my_variables_map["PAGE_ID"] = \
-            response.json()["results"][0]["parent"]["page_id"]
         # Database Entries
         url = f"https://api.notion.com/v1/databases/"\
               f"{self.my_variables_map['DATABASE_ID']}/query"
@@ -54,15 +47,19 @@ class MyIntegration:
         resp = response.json()
         for v in resp["results"]:
             self.my_variables_map["NOTION_ENTRIES"].update({
-                v["properties"]["Product Id"]["rich_text"][0]["plain_text"]: {
+                v["properties"]["id"]["rich_text"][0]["plain_text"]: {
                     "Sales Count": v["properties"]["Sales Count"]["number"],
-                    "Product": v["properties"]["Product"]
-                    ["title"][0]["text"]["content"],
-                    "Link": v["properties"]["Link"]["url"], "PageId": v["id"]
+                    "Price": v["properties"]["Price"]["number"],
+                    "Revenue": v["properties"]["Revenue"]["number"],
+                    "Link": v["properties"]["Link"]["url"],
+                    "Name": v["properties"]["Name"]["title"][0]["plain_text"],
+                    "id": v["properties"]["id"]["rich_text"][0]["plain_text"],
+                    "cover": v["cover"]["external"]["url"],
+                    "page_id": v['id']
                 }
             })
 
-    def getGumroadProducts(self):
+    def get_gumroad_products(self):
         url = "https://api.gumroad.com/v2/products/"
         headers = {
             'Authorization': 'Bearer ' +
@@ -70,31 +67,30 @@ class MyIntegration:
         }
         response = requests.request("GET", url, headers=headers)
         for i in response.json()["products"]:
-            self.updateNotionEntries(i)
+            self.update_notion_entries(i)
 
-    def updateNotionEntries(self, data):
-        for k, v in self.my_variables_map["NOTION_ENTRIES"].items():
-            if data["id"] == k:
-                v.update({
-                    "Sales Count": data["sales_count"],
-                    "Product": data["name"],
-                    "Link": data["short_url"],
-                    "Published": data["published"]
-                })
-                return
+    def update_notion_entries(self, data):
+        or_data_page_id = self.my_variables_map["NOTION_ENTRIES"].get(
+            data['id'], {}
+        ).get('page_id', None)
         self.my_variables_map["NOTION_ENTRIES"].update({
-                data["id"]: {
-                    "Sales Count": data["sales_count"],
-                    "Product": data["name"],
-                    "Link": data["short_url"],
-                    "Published": data["published"]
-                    }
-                }
-            )
+            data["id"]: {
+                "Name": data["name"],
+                "Price": float(data["price"]) / 100,
+                "Sales Count": data["sales_count"],
+                "Revenue": float(data["sales_usd_cents"]) / 100,
+                "Link": data["short_url"],
+                "id": data["id"],
+                "cover": data.get(
+                    "preview_url", self.fallback_cover_image_url
+                ) or self.fallback_cover_image_url,
+                "page_id": or_data_page_id
+            }
+        })
 
-    def updateNotionDatabase(self, pageId, databaseId, productId, data):
-        if pageId:
-            url = "https://api.notion.com/v1/pages/" + str(pageId)
+    def update_notion_database(self, database_id, data):
+        if data.get("page_id"):
+            url = "https://api.notion.com/v1/pages/" + str(data.get("page_id"))
             method = "PATCH"
         else:
             url = "https://api.notion.com/v1/pages/"
@@ -102,117 +98,104 @@ class MyIntegration:
         headers = {
             'Authorization':
                 'Bearer ' + self.my_variables_map["MY_NOTION_SECRET_TOKEN"],
-            'Notion-Version': '2021-05-13',
+            'Notion-Version': '2022-02-22',
             'Content-Type': 'application/json'
         }
         payload = json.dumps({
+            "cover": {
+                "type": "external",
+                "external": {
+                    "url": data["cover"]
+                }
+            },
+            "parent": {
+                "database_id": database_id
+            },
             "properties": {
-                "Product Id": {
+                "Sales Count": {
+                        "type": "number",
+                        "number": float(data["Sales Count"])
+                },
+                "Revenue": {
+                    "type": "number",
+                    "number": float(data["Revenue"])
+                },
+                "id": {
+                    "type": "rich_text",
                     "rich_text": [
                         {
                             "type": "text",
                             "text": {
-                                "content": productId,
+                                "content": data["id"],
+                                "link": None
                             },
-                            "plain_text": productId,
+                            "annotations": {
+                                "bold": False,
+                                "italic": False,
+                                "strikethrough": False,
+                                "underline": False,
+                                "code": False,
+                                "color": "default"
+                            },
+                            "plain_text": data["id"],
+                            "href": None
                         }
                     ]
-                },
-                "Sales Count": {
-                    "number": data["Sales Count"]
                 },
                 "Link": {
                     "type": "url",
                     "url": data["Link"]
                 },
-                "Creator": {
-                    "type": "url",
-                    "url": urlparse(data["Link"]).netloc
+                "Price": {
+                    "type": "number",
+                    "number": float(data["Price"])
                 },
-                "Published": {
-                    "type": "checkbox",
-                    "checkbox": data["Published"]
-                },
-                "Product": {
-                    "title": [
-                        {
-                            "text": {
-                                "content": data["Product"],
-                            },
-                            "plain_text": data["Product"],
-                        }
-                    ]
-                }
-            },
-            "parent": {
-                "type": "database_id",
-                "database_id": databaseId,
-            }
-        })
-        return requests.request(
-                method, url, headers=headers, data=payload
-            ).json()["id"]
-
-    def updatePageTitle(self):
-        """
-        This is special!
-        """
-        sales = 0
-        self.getPageAndDatabaseData()
-        for _, data in self.my_variables_map["NOTION_ENTRIES"].items():
-            sales += data["Sales Count"]
-        url = f"https://api.notion.com/v1/pages/"\
-              f"{self.my_variables_map['PAGE_ID']}"
-        payload = json.dumps({
-            "properties": {
-                "title": {
+                "Name": {
                     "id": "title",
                     "type": "title",
                     "title": [
                         {
                             "type": "text",
                             "text": {
-                                "content":
-                                f"👤 `{self.my_variables_map['GUMROAD_USER']['name']}`"
-                                f" made {sales} Gumroad Product Sales",
-                                "link": {
-                                        "url": f"{self.my_variables_map['GUMROAD_USER']['profile']}"
-                                    }
+                                "content": data["Name"],
+                                "link": None
                             },
+                            "annotations": {
+                                "bold": False,
+                                "italic": False,
+                                "strikethrough": False,
+                                "underline": False,
+                                "code": False,
+                                "color": "default"
+                            },
+                            "plain_text": data["Name"],
+                            "href": None
                         }
                     ]
                 }
             }
         })
-        headers = {
-            'Notion-Version': '2021-05-13',
-            'Authorization': 'Bearer ' + self.my_variables_map["MY_NOTION_SECRET_TOKEN"],
-            'Content-Type': 'application/json'
-        }
-        requests.request("PATCH", url, headers=headers, data=payload)
+        response = requests.request(method, url, headers=headers, data=payload)
+        return response.json()["id"]
 
-    def UpdateIndefinitely(self):
+    def update_indefinitely(self):
         while True:
             try:
-                self.getGumroadProducts()
-                for productId, data in self.my_variables_map["NOTION_ENTRIES"].items():
-                    print(data)
-                    data["PageId"] = self.updateNotionDatabase(
-                        pageId=data["PageId"] if "PageId" in data else None,
-                        productId=productId,
+                self.get_gumroad_products()
+                for _, data in self.my_variables_map["NOTION_ENTRIES"].items():
+                    data["page_id"] = self.update_notion_database(
                         data=data,
-                        databaseId=self.my_variables_map["DATABASE_ID"]
+                        database_id=self.my_variables_map["DATABASE_ID"]
                     )
                     time.sleep(5)
-                self.updatePageTitle()
                 time.sleep(10)
-            except Exception as e:
-                print(f"[Error encountered]: {e}")
+            except Exception:
+                traceback.print_exception(*sys.exc_info())
                 # Drop memory and rebuild from existing notion server state
                 self.my_variables_map["NOTION_ENTRIES"] = {}
-                self.getPageAndDatabaseData()
+                self.get_page_and_database_data()
 
 
 if __name__ == "__main__":
     # With 😴 sleeps to prevent rate limit from kicking in.
-    MyIntegration().UpdateIndefinitely()
+    MyIntegration().update_indefinitely()
